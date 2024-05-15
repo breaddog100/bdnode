9e983f9fe0dcf6f941906bc2711e417ef6011392
kimi <kimi@kimideMacBook-Air.local>
2024-05-15 12:57:53 +0800

    add Quil

--- bdnode.sh
+++ bdnode.sh
@@ -16,11 +16,12 @@ function main_menu() {
         echo "106. Lava 一键部署"
         echo "108. Privasea 一键部署"
         echo "109. Taiko Hekla 一键部署"
-        echo "110. Titan Network 一键部署"
         echo "111. Artela 一键部署"
         echo "112. Tanssi Network 一键部署"
+        echo "113. Quilibrium Network 一键部署"
         echo "--------------------挖矿类项目--------------------"
         echo "503. Spectre(CPU) 一键挖矿"
+        echo "110. Titan Network 一键挖矿"
         echo "---------------------已停项目---------------------"
         echo "107. Taiko 一键部署[已停用]"
         echo "501. ORE(CPU) -v1 挖矿脚本[已停用]"
@@ -36,15 +37,14 @@ function main_menu() {
         104) wget -O Alignedlayer.sh https://raw.githubusercontent.com/breaddog100/AlignedLayer/main/Alignedlayer.sh && chmod +x Alignedlayer.sh && ./Alignedlayer.sh ;;
         105) wget -O Fuel.sh https://raw.githubusercontent.com/breaddog100/fuel/main/fuel-v1.0.sh&& chmod +x Fuel.sh && ./Fuel.sh ;;
         106) wget -O Lava.sh https://raw.githubusercontent.com/breaddog100/lava/main/lava-v1.0.sh && chmod +x Lava.sh && ./Lava.sh ;;
-        
         108) wget -O Privasea.sh https://raw.githubusercontent.com/breaddog100/privasea/main/Privasea.sh && chmod +x Privasea.sh && ./Privasea.sh ;;
         109) wget -O taiko-hekla.sh https://raw.githubusercontent.com/breaddog100/taiko/main/taiko-hekla.sh && chmod +x taiko-hekla.sh && ./taiko-hekla.sh ;;
-        110) wget -O titan-network.sh https://raw.githubusercontent.com/breaddog100/titan-network/main/titan-network.sh && chmod +x titan-network.sh && ./titan-network.sh ;;
         111) wget -O Artela.sh https://raw.githubusercontent.com/breaddog100/artela/main/Artela.sh && chmod +x Artela.sh && ./Artela.sh ;;
         112) wget -O tanssinetwork.sh https://raw.githubusercontent.com/breaddog100/tanssi/main/tanssinetwork.sh && chmod +x tanssinetwork.sh && ./tanssinetwork.sh ;;
+        113) wget -O quil.sh https://raw.githubusercontent.com/breaddog100/QuilibriumNetwork/main/quil.sh && chmod +x quil.sh && ./quil.sh ;;
         
         503) wget -O spectre.sh https://raw.githubusercontent.com/breaddog100/spectre-network/main/spectre.sh && chmod +x spectre.sh && ./spectre.sh ;;
-        
+        110) wget -O titan-network.sh https://raw.githubusercontent.com/breaddog100/titan-network/main/titan-network.sh && chmod +x titan-network.sh && ./titan-network.sh ;;
         
         0) echo "退出脚本。"; exit 0 ;;
 	    *) echo "无效选项，请重新输入。"; sleep 3 ;;

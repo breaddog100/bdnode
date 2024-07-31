9fe1b4c05d8566c36ca775a80019b3abcdab7080
kimi <kimi@kimideMacBook-Air.local>
2024-08-01 00:09:36 +0800

    update

--- update.sh
+++ update.sh
@@ -1,25 +1,33 @@
 #!/bin/bash
 
 # 设置版本号
-current_version=20240731008
+current_version=20240731009
 
 # 指定URL
 update_url="https://raw.githubusercontent.com/breaddog100/bdnode/main/update.sh"
+file_name=$(basename "$update_url")
 
 # 定义函数
 update_script() {
     # 下载脚本文件
-    curl -sO -H "Cache-Control: no-cache" "$update_url"
+    tmp=(date +%s)
+    curl -s -o $HOME/$tmp -H "Cache-Control: no-cache" "$update_url?$tmp"
+
 
     # 检查是否有新版本可用
-    latest_version=$(grep -oP 'current_version=([0-9]+)' update.sh | sed -n 's/.*=//p')
+    latest_version=$(grep -oP 'current_version=([0-9]+)' $HOME/$tmp | sed -n 's/.*=//p')
 
     if [[ "$latest_version" != "$current_version" ]]; then
         # 提示需要更新脚本
         printf "\033[31m脚本有新版本可用！当前版本：%s，最新版本：%s\033[0m\n" "$current_version" "$latest_version"
+        mv $HOME/$tmp $HOME/$file_name
+        chmod +x $HOME/$file_name
+        exit
+        exec $HOME/$file_name
     else
         # 脚本是最新的
         echo "脚本已是最新版本"
+        rm -f $tmp
     fi
 
 }
